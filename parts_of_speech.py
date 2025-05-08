8cf140feedf7b4651b1293bf2e1461e1fd2042d3
umbetovalmaz <umbetov.almaz.al@gmail.com>
2025-05-08 20:59:56 +0500

    Added parts of speech

---
+++ parts_of_speech.py
@@ -0,0 +1,31 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+
+POS_MARKINGS = {
+    'VERB' : 'Verbs',
+    'NOUN' : 'Nouns',
+    'PRON' : 'Pronouns',
+    'ADJ' : 'Adjectives',
+    'ADV' : 'Adverbs',
+    'ADP' : 'Adpositions',
+    'CONJ' : 'Conjunctions',
+    'DET' : 'Determiners',
+    'NUM' : 'Cardinal',
+    'PRT' : 'Particles',
+    'X' : 'Other',
+}
+
+def parts_of_speech(text: str):
+    words = word_tokenize(text, "english")
+    tagged = pos_tag(words, tagset='universal')
+    result = {}
+
+    for word, tag in tagged:
+        if tag == '.':
+            continue 
+        else:
+            normal_tag = POS_MARKINGS[tag]
+        result.setdefault(normal_tag, []).append(word)
+
+
