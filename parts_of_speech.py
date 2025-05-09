f72d35ac6c20f17457ad9b1450d296eea46f1c7d
Bahodir Madatov <n.b.madatov@gmail.com>
2025-05-09 15:45:25 +0500

    bugfix

--- parts_of_speech.py
+++ parts_of_speech.py
@@ -17,15 +17,34 @@ POS_MARKINGS = {
 }
 
 def parts_of_speech(text: str) -> Dict[str, List[str]]:
-    words = word_tokenize(text, "english")
-    tagged = pos_tag(words, tagset='universal')
+    words = word_tokenize(text)
+    tagged = pos_tag(words)
     result = {}
 
     for word, tag in tagged:
         if tag == '.':
             continue 
         else:
-            normal_tag = POS_MARKINGS[tag]
+            if tag.startswith('NN'):
+                normal_tag = 'Nouns'
+            elif tag.startswith('VB'):
+                normal_tag = 'Verbs'
+            elif tag.startswith('JJ'):
+                normal_tag = 'Adjectives'
+            elif tag.startswith('RB'):
+                normal_tag = 'Adverbs'
+            elif tag.startswith('PRP'):
+                normal_tag = 'Pronouns'
+            elif tag.startswith('IN'):
+                normal_tag = 'Adpositions'
+            elif tag.startswith('CC'):
+                normal_tag = 'Conjunctions'
+            elif tag.startswith('DT'):
+                normal_tag = 'Determiners'
+            elif tag.startswith('CD'):
+                normal_tag = 'Cardinal'
+            else:
+                normal_tag = 'Other'
         result.setdefault(normal_tag, []).append(word)
     return result
 
