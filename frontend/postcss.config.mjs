e57c79763d70e1765588f1651df5a809485442d4
CYBERTSTALKER07 <shakhzodsoliyevgates@gmail.com>
2025-05-22 19:23:14 +0500

    ui

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,6 @@
-// postcss.config.js (or .cjs)
-module.exports = {
+export default {
   plugins: {
-    // Use the require('@tailwindcss/postcss') syntax
-    '@tailwindcss/postcss': {}, // This is often the fix for "tailwindcss directly as a PostCSS plugin"
-    autoprefixer: {}, // Only if you also use autoprefixer
+    tailwindcss: {},
+    autoprefixer: {},
   },
-};
+}
