e0c0bfab650c1d3d9a2063bb74717de7e5cc8980
vika <gabaibaibayok@gmail.com>
2025-06-25 19:53:24 +0500

    replaced api key

--- done_with_some_llm/sapling.py
+++ done_with_some_llm/sapling.py
@@ -1,16 +1,18 @@
 import requests
 from pprint import pprint
+import os
+
+key = os.getenv("SAPLING_KEY")
 
 def get_tone(text):
     response = requests.post(
         "https://api.sapling.ai/api/v1/tone",
         json={
-            "key": "015DQLX8TMB98ZT4L39YZT1Y735MOGOG",
+            "key": key,
             "text": text
         }
     )
 
     oval = response.json()["overall"]
-
 
     return oval
